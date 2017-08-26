c8a042ddac787e2e5e30485ae0a709467ca0a4e9
mackswane <31156711+mackswane@users.noreply.github.com>
2017-08-26 13:15:28 +0530

    Create lab4.cpp

---
+++ lab4.cpp
@@ -0,0 +1,18 @@
+# lab2
+//question 4
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ float d,y,w;
+
+    cout <<"enter number of days="<< endl;
+    cin>>d;
+    y=d/365;
+    w=d/7;
+    cout<<"\nyears="<<y;
+    cout<<"\nweeks="<<w;
+    return 0;
+}
