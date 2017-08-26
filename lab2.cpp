85e0b8ee174622a83c43c2ad543fbaa265c18a25
mackswane <31156711+mackswane@users.noreply.github.com>
2017-08-26 12:31:02 +0530

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,18 @@
+# lab2
+//question 1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ float l,m,km;
+
+    cout <<"enter length in centimeter="<< endl;
+    cin>>l;
+    m=l/100;
+    km=l/100000;
+    cout<<"\nlength in meter="<<m;
+    cout<<"\nlength in kilometer="<<km;
+    return 0;
+}
