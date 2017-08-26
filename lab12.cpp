9c9fcfb867a8f62c5bb275bd6d59fc52f224476a
mackswane <31156711+mackswane@users.noreply.github.com>
2017-08-26 14:20:50 +0530

    Create lab12.cpp

---
+++ lab12.cpp
@@ -0,0 +1,25 @@
+# lab2
+//question 12
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+ float p,r,t,n,x,y,z,ci;
+
+    cout <<"\nenter principal amount=";
+    cin>>p;
+    cout <<"\nenter annual interest rate =";
+    cin>>r;
+    cout <<"\nenter time in years =";
+    cin>>t;
+    cout<<"\nenter no. of times interest is compounded per year=";
+    cin>>n;
+    y=n*t;
+    x=1+(r/n);
+    z=pow(x,y);
+    ci=p*z;
+    cout <<"\ncompound interest="<<ci;
+    return 0;
+}
