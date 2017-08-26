81811a68fa5f229c9cca146503ab5d464b62614b
mackswane <31156711+mackswane@users.noreply.github.com>
2017-08-26 13:31:07 +0530

    Create lab8.cpp

---
+++ lab8.cpp
@@ -0,0 +1,18 @@
+# lab2
+//question 8
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ float a,b,h;
+
+
+    cout <<"\nenter base of triangle =";
+    cin>>b;
+    cout <<"\nenter height of triangle =";
+    cin>>h;
+    a=(b*h)/2;
+    cout<<"\narea of triangle  ="<<a;
+    return 0;
+}
