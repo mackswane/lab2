c2789f65969ac7f568716ad60bf0030ab223f6c0
mackswane <31156711+mackswane@users.noreply.github.com>
2017-08-26 13:21:06 +0530

    Create lab5.cpp

---
+++ lab5.cpp
@@ -0,0 +1,18 @@
+# lab2
+//question 5
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+ float x,y,v;
+
+    cout <<"\nenter a number =";
+    cin>>x;
+    cout<<"\nenter the power of no.=";
+    cin>>y;
+    v=pow(x,y);
+    cout<<"\nvalue="<<v;
+    return 0;
+}
